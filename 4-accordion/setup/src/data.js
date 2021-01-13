fbdc2b3161df8548c7c38f2fc95b112f79c514dd
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-13 23:20:53 +0100

    update

--- 4-accordion/setup/src/data.js
+++ 4-accordion/setup/src/data.js
@@ -29,5 +29,11 @@ const questions = [
     info:
       'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
   },
+  {
+    id: 6,
+    title: 'I co dalej',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+  },
 ]
 export default questions
